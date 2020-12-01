f0a6104a11b4c2cd4562691196b53566c3e63b39
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2020-12-01 22:23:19 +0600

    Load Phioto,Delete Photo(admin site)
    Show photo,set pagination using load more button,also delete photo from database aslo from local storage ...

--- admin/app/Http/Controllers/PhotoController.php
+++ admin/app/Http/Controllers/PhotoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\PhotoModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PhotoController extends Controller
 {
@@ -11,11 +12,38 @@ class PhotoController extends Controller
         return view('Photo');
     }
 
+    function PhotoJSON(){
+        return PhotoModel::take(4)->get();
+    }
+
+    function  PhotoDelete(Request $request){
+        $OldPhotoURL=$request->input('OldPhotoURL');
+        $OldPhotoID=$request->input('id');
+
+        $OldPhotoURLArray=explode("/",$OldPhotoURL);
+        $OldPhotoName=end($OldPhotoURLArray);
+
+        $DeletePhotoFile=Storage::delete('public/'.$OldPhotoName);
+        $DeleteRow=PhotoModel::where('id',$OldPhotoID)->delete();
+
+        if($DeleteRow ==true){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    function PhotoJSONByID(Request $request){
+        $FirstID=$request->id;
+        $LastID=$FirstID+4;
+        return PhotoModel::where('id','>=',$FirstID)->where('id','<',$LastID)->get();
+    }
+
     function PhotoUpload(Request $request){
         $photoPath=$request->file('photo')->store('public');
         $photoName=(explode('/',$photoPath))[1];
         $host=$_SERVER['HTTP_HOST'];
-        $location=$host."/storage/".$photoName;
+        $location="http://".$host."/storage/".$photoName;
         $result = PhotoModel::insert(['location'=>$location]);
 
         if($result ==true){
--- admin/resources/views/Photo.blade.php
+++ admin/resources/views/Photo.blade.php
@@ -9,9 +9,13 @@
                 <button data-toggle="modal" data-target="#PhotoModal" id="addNewPhotoBtnId" class="btn my-3 btn-sm btn-danger">Add New </button>
             </div>
         </div>
+    </div>
+
+    <div class="container-fluid ">
         <div class="row photoRow">
 
         </div>
+        <button class="btn btn-primary" id="LoadMoreBtn">Load More</button>
     </div>
 
     <!-- Modal -->
@@ -38,44 +42,166 @@
             </div>
         </div>
     </div>
+
+    <!--Delete Modal -->
+    <div class="modal fade" id="deletePhotoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body p-3 text-center">
+                    <h5 class="mt-4">Do You Want To Delete</h5>
+                    <h6 id="PhotoDeleteId" class="mt-4 d-none"></h6>
+                    <h6 id="PhotoDeleteLocation" class="mt-4 d-none"></h6>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">No</button>
+                    <button  id="PhotoDeleteConfirmBtn" type="button" class="btn btn-sm btn-danger">Yes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Delete Modal end -->
 @endsection
 
 @section('script')
     <script type="text/javascript">
-        $('#imgInput').change(function (){
-            var reader=new FileReader();
+        $('#imgInput').change(function() {
+            var reader = new FileReader();
             reader.readAsDataURL(this.files[0]);
-            reader.onload=function (event){
-                var ImgSource=event.target.result;
+            reader.onload = function(event) {
+                var ImgSource = event.target.result;
                 $('#imgPreview').addClass('imgPreview');
-                $('#imgPreview').attr('src',ImgSource);
+                $('#imgPreview').attr('src', ImgSource);
             }
         });
 
-        $('#SavePhoto').on('click',function (){
+        $('#SavePhoto').on('click', function() {
 
             $('#SavePhoto').html("<div class=\'spinner-border spinner-border-sm\' role=\'status\'></div>");
 
-            var PhotoFile=$('#imgInput').prop('files')[0];
+            var PhotoFile = $('#imgInput').prop('files')[0];
             var formData = new FormData();
-            formData.append('photo',PhotoFile);
+            formData.append('photo', PhotoFile);
 
-            axios.post("/photoUpload",formData).then(function (response){
-                if(response.status==200 && response.data==1){
+            axios.post("/photoUpload", formData).then(function(response) {
+                if (response.status == 200 && response.data == 1) {
                     $('#SavePhoto').html("Save");
                     toastr.success('Photo Uploaded Successfully');
                     $('#PhotoModal').modal('hide');
-                }else{
+                    window.location.href = "/photo";
+                } else {
                     $('#PhotoModal').modal('hide');
                     toastr.error('Photo Upload Failed !');
+                    window.location.href = "/photo";
                 }
 
-            }).catch(function (error){
+            }).catch(function(error) {
                 $('#PhotoModal').modal('hide');
                 toastr.error('Photo Upload Failed !');
                 $('#SavePhoto').html("Save");
+                window.location.href = "/photo";
             })
 
-        });
+        })
+
+        LoadPhoto();
+        //To load photo from the db
+        function LoadPhoto() {
+
+            axios.get("/PhotoJSON").then(function(response) {
+
+                $.each(response.data, function(i, item) {
+                    $("<div class='col-md-3 p-1'>").html(
+                        "<img data-id=" + item['id'] + " class='imgOnRow' src=" + item['location'] + ">" +
+                        "<button data-id=" + item['id'] + "data-photo=" + item['location'] + " class='btn btn-sm deletePhoto'>Delete</button>"
+                    ).appendTo('.photoRow');
+                })
+
+                //delete button
+                $('.deletePhoto').on('click', function(event) {
+                    let id = $(this).data('id');
+                    let photo = $(this).data('photo');
+
+                    $('#PhotoDeleteId').html(id);
+                    $('#PhotoDeleteLocation').html(photo);
+
+                    $('#deletePhotoModal').modal('show');
+                    //PhotoDelete(photo,id);
+                    //event.preventDefault();
+                });
+
+            }).catch(function(error) {
+
+            })
+        }
+
+
+        //to load more images
+        var ImgID = 0;
+
+        function LoadByID(FirstImgID, loadMoreBtn) {
+            ImgID = ImgID + 4;
+            let PhotoID = ImgID + FirstImgID;
+            let url = '/PhotoJSONByID/' + PhotoID;
+            loadMoreBtn.html("<div class=\'spinner-border spinner-border-sm\' role=\'status\'></div>");
+            axios.get(url).then(function(response) {
+                loadMoreBtn.html("Load More");
+                $.each(response.data, function(i, item) {
+                    $("<div class='col-md-3 p-1'>").html(
+                        "<img data-id=" + item['id'] + " class='imgOnRow' src=" + item['location'] + ">" +
+                        "<button data-id=" + item['id'] + " data-photo=" + item['location'] + " class='btn btn-sm deletePhoto'>Delete</button>"
+                    ).appendTo('.photoRow');
+
+                    //delete button
+                    $('.deletePhoto').on('click', function(event) {
+                        let id = $(this).data('id');
+                        let photo = $(this).data('photo');
+
+                        $('#PhotoDeleteId').html(id);
+                        $('#PhotoDeleteLocation').html(photo);
+
+                        $('#deletePhotoModal').modal('show');
+                        //PhotoDelete(photo,id);
+                        //event.preventDefault();
+                    });
+                });
+            }).catch(function(error) {
+                loadMoreBtn.html("Load More");
+            })
+        }
+
+        //to catch first id of the image in the Loadmore button
+        $('#LoadMoreBtn').on('click', function() {
+            loadMoreBtn = $(this);
+            let FirstImgID = $(this).closest('div').find('img').data('id');
+
+            LoadByID(FirstImgID, loadMoreBtn);
+        })
+
+        //delete confirmation btn
+        $('#PhotoDeleteConfirmBtn').click(function(event) {
+            let id = $('#PhotoDeleteId').html();
+            let photo = $('#PhotoDeleteLocation').html();
+            PhotoDelete(photo, id);
+            event.preventDefault();
+        })
+
+        //to delete photo
+        function PhotoDelete(OldPhotoURL, id) {
+            let URL = "/PhotoDelete";
+            let MyFormData = new FormData();
+            MyFormData.append('OldPhotoURL', OldPhotoURL);
+            MyFormData.append('id', id);
+            axios.post(URL, MyFormData).then(function(response) {
+                if (response.status == 200 && response.data == 1) {
+                    toastr.success("Photo Deleted Successfully.");
+                    window.location.href = "/photo";
+                } else {
+                    toastr.error("Photo Delete Failed!");
+                }
+            }).catch(function(error) {
+                toastr.error("Photo Delete Failed!");
+            })
+        }
     </script>
 @endsection
